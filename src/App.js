7cff54f5e7c986c61f1031ea2ddc971545dcd881
nikocevicstefan <nikocevicstefan@gmail.com>
2020-04-12 22:24:24 +0200

    Makes base project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-     <h1>React</h1>
-    </div>
-  );
+     <h1>Stefan Nikocevic</h1>
+    </div>  );
 }
 
 export default App;
