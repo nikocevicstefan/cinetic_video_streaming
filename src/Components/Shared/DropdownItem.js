5b50134274c46c621f456de15ea62710090cbaa3
nikocevicstefan <nikocevicstefan@gmail.com>
2020-04-18 20:17:21 +0200

    Small dropdown fix

--- src/Components/Shared/DropdownItem.js
+++ src/Components/Shared/DropdownItem.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const DropdownItem = (props) => {
+    const toggleDropdown = (e) => {e.target.nextSibling.classList.toggle('false');};
+
     const {shown, hidden} = props;
     return (
         <div className="dropdown-item">
-            <h3 className="dropdown-item__shown">{shown}</h3>
+            <h3 className="dropdown-item__shown" onClick={toggleDropdown}>{shown}</h3>
             <p className="dropdown-item__hidden">{hidden}</p>
         </div>
     );
